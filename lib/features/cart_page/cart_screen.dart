37106c59adbcf19069f6699d245a579fd04673ca
Timka <vendetta10090@gmail.com>
2024-04-25 23:20:22 +0500

    add screens

--- lib/features/cart_page/cart_screen.dart
+++ lib/features/cart_page/cart_screen.dart
@@ -5,6 +5,6 @@ class CartScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(child: Text("this is cart page!"));
   }
 }
--- lib/features/favorites_page/favorites_screen.dart
+++ lib/features/favorites_page/favorites_screen.dart
@@ -5,6 +5,6 @@ class FavoritesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Center(child: Text("this is favorites page!"));
   }
 }
